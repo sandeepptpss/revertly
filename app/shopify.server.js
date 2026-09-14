501ab6bade73c6c27298780a28b92eaa6a00d7b5
sandeepptpss <sandeepptpss@gmail.com>
2026-09-14 15:35:37 +0530

    update code

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -6,7 +6,7 @@ import {
   shopifyApp,
 } from "@shopify/shopify-app-react-router/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import prisma from "./db.server";
+import prisma from "./db.server.js";
 
 export const PLAN_STARTER = "Starter";
 export const PLAN_GROWTH = "Growth";
